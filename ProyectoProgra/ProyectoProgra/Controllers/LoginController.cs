7e3a1959388f8f752cc76a2e688f4e3981211e4e
Jose Daniel Mata <jdmata16@gmail.com>
2023-11-16 20:23:10 -0600

    Registrar mas su diseño

---
+++ ProyectoProgra/ProyectoProgra/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using ProyectoProgra.Entities;
+using ProyectoProgra.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoProgra.Controllers
+{
+    public class LoginController : Controller
+    {
+        UsuarioModel modelUsuario = new UsuarioModel();
+        [HttpGet]
+        public ActionResult RegistrarCuenta()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegistrarCuenta(UsuarioEnt entidad)
+        {
+            string respuesta = modelUsuario.RegistrarCuenta(entidad);
+
+            if (respuesta == "OK")
+            {
+                return RedirectToAction("RegistrarCuenta", "Login");
+            }
+            else
+            {
+                ViewBag.MensajeUsuario = "No se ha podido registrar su información";
+                return View();
+            }
+        }
+    }
+}
